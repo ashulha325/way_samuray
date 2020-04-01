068f9d5075091dbdd4977680de0c51587847a362
Andriy <ashulha325@gmail.com>
2020-04-01 18:20:21 +0300

    ajax + thunk + login

---
+++ src/components/Header/HeaderContainer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import  classes from "./Header.module.scss";
+import {NavLink} from "react-router-dom";
+
+const Header = () =>{
+    return(
+        <header className={classes.header}>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Oikya_Front_Logo.png"
+                alt="background-er"
+            />
+            <span>I'ts are beautiful social network</span>
+            <div className={classes.loginBlock}>
+                <NavLink to={"/login"}>Login</NavLink>
+            </div>
+        </header>
+    );
+};
+export  default  Header;
